886dd4495a97ffe860e6e719c8da76b341ed3d2d
ALion Alex <alionalexxx@gamil.com>
2025-05-07 01:55:59 +0300

    feat(log-in): add email/password inputs and submit button with styles

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,16 +1,50 @@
-import { Link } from 'react-router-dom'
+import { RegisterNav } from '@components/RegisterNav/RegisterNav'
+import './LoginPage.scss'
+import { useNavigate } from 'react-router-dom'
 
 export function LoginPage() {
+  const navigate = useNavigate()
+
+  const handleSubmit = (event: React.FocusEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    navigate('/home')
+  }
+
   return (
-    <>
-      <div>LoginPage</div>
-      <span>
-        <Link to="/sign-up">sign-up</Link>
-      </span>
-      <span> </span>
-      <span>
-        <Link to="/home">home</Link>
-      </span>
-    </>
+    <main className="login-page">
+      <div className="login-block">
+        <RegisterNav />
+        <div className="log-in">
+          <div className="log-in__hint">
+            Enter your username and password to login.
+          </div>
+          <form
+            className="log-in__form"
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <input name="email" type="email" required />
+            <input name="password" type="password" required />
+            <div className="log-in__forgetful">Forgot password?</div>
+
+            <button className="log-in__button" type="submit">
+              Sign In
+            </button>
+          </form>
+        </div>
+
+        <div className="log-in-alt">
+          <div className="divider">
+            <span className="divider__text">Or login with</span>
+          </div>
+          <button className="log-in-alt__button" type="submit">
+            Login with Google
+          </button>
+          <button className="log-in-alt__button" type="submit">
+            Login with Facebook
+          </button>
+        </div>
+      </div>
+    </main>
   )
 }
