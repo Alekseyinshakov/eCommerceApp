5b813a9afa507f1d7c623650400b6cf174f50a7c
Inna Fedorova <innamassura@gmail.com>
2025-05-09 21:21:06 +0300

    fix: global interface fail

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -7,18 +7,3 @@ declare module '*.svg' {
   const content: string
   export default content
 }
-
-declare global {
-  interface ImportMeta {
-    env: {
-      VITE_CTP_PROJECT_KEY: string
-      VITE_CTP_SCOPES: string
-      VITE_CTP_API_URL: string
-      VITE_CTP_AUTH_URL: string
-      VITE_CTP_CLIENT_ID: string
-      VITE_CTP_CLIENT_SECRET: string
-    }
-  }
-}
-
-export {}
