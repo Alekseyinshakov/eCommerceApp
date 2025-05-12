427eadc0ac4a0eb8dca940ca8678d24aec730163
Inna Fedorova <innamassura@gmail.com>
2025-05-12 22:43:29 +0300

    feat: burger-menu and change styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import styles from './Header.module.scss'
 import { useAuthStore } from '@store/authStore'
+import { useEffect, useRef, useState } from 'react'
 
-const { header, logo, navList, navItem, activeLink, rightSide, search, cart } =
-  styles
+const {
+  headerContainer,
+  logo,
+  navList,
+  navItem,
+  activeLink,
+  rightSide,
+  search,
+  cart,
+  burgerMenu,
+  open,
+} = styles
 
 function Header() {
   const navigate = useNavigate()
@@ -16,68 +27,98 @@ function Header() {
     navigate('/log-in')
   }
 
+  const [isOpen, setIsOpen] = useState(false)
+  const navRef = useRef<HTMLUListElement>(null)
+
+  useEffect(() => {
+    const handleMenuClose = (event: MouseEvent) => {
+      if (navRef.current && !navRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener('mousedown', handleMenuClose)
+    return () => {
+      document.removeEventListener('mousedown', handleMenuClose)
+    }
+  }, [])
+
   return (
-    <header className={header}>
-      <img className={logo} src="images/Logo.svg" alt="logo" />
-      <nav>
-        <ul className={navList}>
-          <li className={navItem}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : '')}
-              to="/home"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className={navItem}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : '')}
-              to="/shop"
-            >
-              Shop
-            </NavLink>
-          </li>
-          <li className={navItem}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : '')}
-              to="/about"
-            >
-              About
-            </NavLink>
-          </li>
-          <li className={navItem}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : '')}
-              to="/plant-care"
-            >
-              Plant Care
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <div className={rightSide}>
-        <div className={search}>
-          <img src="images/icons/find-icon.svg" alt="search" />
+    <header>
+      <div className={`container ${headerContainer}`}>
+        <div className={logo}>
+          <NavLink to="/home">
+            <img src="images/Logo.svg" alt="logo" width={150} height={35} />
+          </NavLink>
         </div>
-        <Link to="/cart" className={cart}>
-          <img src="images/icons/cart-icon.svg" alt="cart" />
-        </Link>
-        {!email && (
-          <Link to="/log-in" className="button">
-            Login
-          </Link>
-        )}
-        {!email && (
-          <Link to="/sign-up" className="button">
-            Register
-          </Link>
-        )}
-        <div className={styles.userEmail}>{email}</div>
-        {email && (
-          <button onClick={handleLogout} className={`button btn-logout`}>
-            Logout
+        <nav className={`${isOpen ? open : ''}`} ref={navRef}>
+          <ul className={navList}>
+            <li className={navItem}>
+              <NavLink
+                className={({ isActive }) => (isActive ? activeLink : '')}
+                to="/home"
+                onClick={() => setIsOpen(false)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={navItem}>
+              <NavLink
+                className={({ isActive }) => (isActive ? activeLink : '')}
+                to="/shop"
+                onClick={() => setIsOpen(false)}
+              >
+                Shop
+              </NavLink>
+            </li>
+            <li className={navItem}>
+              <NavLink
+                className={({ isActive }) => (isActive ? activeLink : '')}
+                to="/about"
+                onClick={() => setIsOpen(false)}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className={navItem}>
+              <NavLink
+                className={({ isActive }) => (isActive ? activeLink : '')}
+                to="/plant-care"
+                onClick={() => setIsOpen(false)}
+              >
+                Plant Care
+              </NavLink>
+            </li>
+          </ul>
+          <button
+            className={`${burgerMenu}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span></span>
           </button>
-        )}
+        </nav>
+        <div className={rightSide}>
+          <button className={search}></button>
+          <Link to="/cart" className={cart}>
+            <img src="images/icons/cart-icon.svg" alt="cart" />
+          </Link>
+          {email ? (
+            <>
+              <div className={styles.userEmail}>{email}</div>
+              <button onClick={handleLogout} className="button btn-logout">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/log-in" className="button">
+                Login
+              </Link>
+              <Link to="/sign-up" className="button">
+                Register
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </header>
   )
