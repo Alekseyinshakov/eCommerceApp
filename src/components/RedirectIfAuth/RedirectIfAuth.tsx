a852bf733818c78e19f318dc5d6f8e315a7059c7
Alexey <alexeyinshakov992@gmail.com>
2025-05-17 11:46:06 +0300

    fix: update usage from auth.email to auth.user

--- src/components/RedirectIfAuth/RedirectIfAuth.tsx
+++ src/components/RedirectIfAuth/RedirectIfAuth.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom'
 import { useAuthStore } from '@store/authStore'
 
 const RedirectIfAuth = ({ children }: { children: React.ReactNode }) => {
-  const isAuthenticated = !!useAuthStore((state) => state.email)
+  const isAuthenticated = !!useAuthStore((state) => state.user)
   return isAuthenticated ? <Navigate to="/home" replace /> : <>{children}</>
 }
 
