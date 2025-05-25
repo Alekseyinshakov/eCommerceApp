f636269cc6b576d5e211a7e155233fd9c8708c1a
Alexey <alexeyinshakov992@gmail.com>
2025-05-25 16:39:00 +0300

    feat: add basic layout

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react'
 import styles from './ProfilePage.module.scss'
 
 export const ProfilePage = () => {
+  const [fields, setFields] = useState([
+    { label: 'First name:', value: 'John' },
+    { label: 'Last name:', value: 'Doe' },
+    { label: 'Date of birth:', value: '1990-01-01' },
+    { label: 'Email:', value: 'jd@mail.com' },
+  ])
+  setFields([])
+  useEffect(() => {
+    return () => {}
+  }, [])
+
   return (
     <div className="container">
-      <h2 className={styles.title}>Profile page</h2>
-      <p className={styles.text}>Welcome to profile page!</p>
-      <div className={styles.row}>
-        <span className={styles.fieldName}>First name: </span>
-        {'Igor'}
-      </div>
-      <div className={styles.row}>
-        <span className={styles.fieldName}>Last name: </span>
-        {'Catamaranov'}
+      <div className={styles.inner}>
+        <h2 className={styles.title}>Profile page</h2>
+        <div className={styles.colWrap}>
+          <ul className={styles.col1}>
+            {fields.map((item, i) => {
+              return (
+                <li key={i} className={styles.fieldName}>
+                  {item.label}
+                </li>
+              )
+            })}
+          </ul>
+          <ul className={styles.col2}>
+            {fields.map((item, i) => {
+              return (
+                <li key={i} className={styles.fieldValue}>
+                  {item.value}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className={styles.buttonsWrap}>
+          <button>Edit</button>
+          <button>Save</button>
+          <button>Cancel</button>
+        </div>
       </div>
     </div>
   )
